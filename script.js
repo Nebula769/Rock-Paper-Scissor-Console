eba76458d2eef5bcd51412c69c0b35e9224b19fa
Haley Zheng <haleyzheng917@gmail.com>
2024-12-09 00:06:38 -0600

    add game logic and try again

--- script.js
+++ script.js
@@ -17,6 +17,33 @@ function getComputerChoice() {
   }
 }
 
+function incrementScore(player) {
+  if (player === 'human') {
+    humanScore++;
+    score = document.getElementById('humanScore');
+    score.textContent = humanScore;
+  } else {
+    computerScore++;
+    score = document.getElementById('computerScore');
+    score.textContent = computerScore;
+  }
+  
+  if (humanScore === 5 || computerScore === 5) {
+    const choices = document.getElementById('choices');
+    choices.style.display = 'none';
+    const result = document.getElementById('result');
+    result.style.color = 'red';
+    if (humanScore === 5) {
+      result.textContent = 'You win the game!';
+    } else {
+      result.textContent = 'You lose the game!';
+    }
+    
+  }
+
+  
+}
+
 function getHumanChoice() {
   let choice =
       prompt('Enter your choice: rock, paper, or scissors').toLowerCase();
@@ -35,27 +62,53 @@ function playRound(humanChoice, computerChoice) {
     return 'It\'s a tie!';
   } else if (humanChoice === 'rock') {
     if (computerChoice === 'paper') {
-      computerScore++;
+      incrementScore("computer");
       return 'You lose! Paper beats rock.';
     } else {
-      humanScore++;
+      incrementScore("human");
       return 'You win! Rock beats scissors.';
     }
   } else if (humanChoice === 'paper') {
     if (computerChoice === 'scissors') {
-      computerScore++;
+      incrementScore("computer");
       return 'You lose! Scissors beats paper.';
     } else {
-      humanScore++;
+      incrementScore("human");
       return 'You win! Paper beats rock.';
     }
   } else {
     if (computerChoice === 'rock') {
-      computerScore++;
+      incrementScore("computer");
       return 'You lose! Rock beats scissors.';
     } else {
-      humanScore++;
+      incrementScore("human");
       return 'You win! Scissors beats paper.';
     }
   }
 }
+
+const choices = document.getElementById('choices');
+choices.addEventListener('click', (e) => {
+  let resultStr;
+  if (e.target.tagName === 'BUTTON') {
+    switch (e.target.id) {
+      case 'rock':
+        resultStr = playRound('rock', getComputerChoice());
+        break;
+      case 'paper':
+        resultStr = playRound('paper', getComputerChoice());
+        break;
+      case 'scissors':
+        resultStr = playRound('scissors', getComputerChoice());
+        break;
+    }
+  }
+  const result = document.getElementById('result');
+  result.textContent = resultStr;
+  result.style.color = 'blue';
+
+});
+
+document.getElementById("tryAgainButton").addEventListener('click', function() {
+  location.reload(); // Reloads the current page
+});
